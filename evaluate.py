d2490410d91316322dea262ec3d1aed5ea53843e
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-04-06 17:41:31 +0200

    Better

--- evaluate.py
+++ evaluate.py
@@ -18,19 +18,21 @@ def evaluate_all():
 
 
 def is_dead(node_name):
-    fname = newest_out_file(node_name)
+    fname = latest_out_file(node_name)
     end_msg = get_end_msg(fname)
     return end_msg != END_MSG
 
 
 
-def newest_out_file(node_name):
+def latest_out_file(node_name):
     job_name = f"DEAD-{node_name[0]}{node_name[-1]}"
     workdir = os.getcwd()
     node_directory = os.path.join(workdir, 'log', job_name)
     all_files = glob.glob(os.path.join(node_directory, '**/*.stdout'), recursive=True)
     print("all_files !!")
     print(all_files)
+    latest_file = max(all_files, key=os.path.getctime)
+    return latest_file
 
 
 def get_end_msg(fname):
